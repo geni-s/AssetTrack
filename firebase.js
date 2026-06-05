8806098ce965d925bd465bb896b0293b630c9e17
geni-s <geniusjareda567@gmail.com>
2026-06-05 17:40:59 +0530

    add request button

--- firebase.js
+++ firebase.js
@@ -12,4 +12,3 @@
 
 
   const app = initializeApp(firebaseConfig);
- 
---
+++ request.js
@@ -0,0 +1,117 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  getDocs,
+    getDoc,
+  addDoc,
+  updateDoc,
+   deleteDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAxMmwGBxQlP6nq99uzuio-BlCBccsM4Zs",
+    authDomain: "al0000.firebaseapp.com",
+    databaseURL: "https://al0000-default-rtdb.firebaseio.com",
+    projectId: "al0000",
+    storageBucket: "al0000.firebasestorage.app",
+    messagingSenderId: "191861001021",
+    appId: "1:191861001021:web:d84b5b8afe5cda67acd354"
+  };
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const ass = document.getElementById("assets");
+let assets = [];
+async function getAss() {
+
+    const querySnapshot = await getDocs(collection(db, "assets"));
+
+    ass.innerHTML = "";
+    assets = [];
+
+    querySnapshot.forEach((doc) => {
+
+        const data = doc.data();
+        assets.push(
+          {id:doc.id,
+           ... data});
+
+        ass.innerHTML += `
+        
+       <div class="assets" id="${doc.id}">
+
+            <h2>Asset Name: ${data.asset}</h2>
+
+            <p>Description: ${data.description}</p>
+
+            <p>Quantity: ${data.quantity}</p>
+            <p>Location: ${data.location}</p>
+             
+             
+        
+
+            <p>Category: ${data.category}</p>
+            <div id="kru">
+            <button class="Reque" onclick="Reque('${doc.id}')">
+              Request
+            </button>
+           
+           
+            </div>
+
+
+        </div>
+        
+        `;
+
+    });
+}
+
+getAss();
+
+
+ window.Reque = async function(id) {
+
+   try {
+
+
+      const assetRef = doc(db, "assets", id);
+
+      const assetSnap = await getDoc(assetRef);
+
+      const assetData = assetSnap.data();
+
+      await addDoc(
+         collection(db, "requests"),
+         {
+            assetId: id,
+            assetName: assetData.asset,
+            category: assetData.category,
+            status: "Pending",
+            requestDate: new Date()
+         }
+      );
+
+      alert("Request Submitted");
+
+   } catch(error) {
+
+      console.log(error);
+      alert("Error deleting ");
+
+   }
+
+}
+
+
+
+
+
+
+
