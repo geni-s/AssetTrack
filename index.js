3e6e0dba3520c9e300d8d53a42c14e2d86bf59f3
geni-s <geniusjareda567@gmail.com>
2026-06-06 14:56:15 +0530

    make admin login

---
+++ index.js
@@ -0,0 +1,15 @@
+const login=document.getElementById("login");
+const adminlogin=document.getElementById("adminlogin");
+
+
+login.addEventListener("click", () => {
+
+    window.location.href = "userlogin.html";
+});
+adminlogin.addEventListener("click", () => {
+
+    window.location.href = "adminlogin.html";
+});
+
+
+
